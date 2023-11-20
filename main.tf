a742bbb796f270c201f47332a1bdcb85c69e69c1
vagrant <vagrant@kali>
2023-11-19 20:30:48 -0500

    s3 bucket is added

--- main.tf
+++ main.tf
@@ -17,12 +17,28 @@ provider "aws" {
 }
 
 #work to do 
+#1 creating EC2
 resource "aws_instance" "nitin_server" {
   ami           = var.AMI_id
   instance_type = var.instance_type
 
   tags = {
     Name = "Nitin_Mehta"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"  # Specify your desired AWS region
+}
+
+#2 creating S3
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "Nitin_bucket"  # Replace with your desired bucket name
+  acl    = "private"  # Access control for the bucket, e.g., "private", "public-read", "public-read-write"
+
+  tags = {
+    Name        = "MyS3Bucket"
+    Environment = ""   # Dev, Prodcution  etc.
   }
 }
 
